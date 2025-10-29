52e52923d504b1ec49a8a33d1fe6a23a63e230b1
Anjana Tinush <146495563+AnjanaTinush@users.noreply.github.com>
2025-10-29 23:36:11 +0530

    fix: update MySQL configuration and connection settings in docker-compose

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST ,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3307,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD ,
   database: process.env.DB_NAME,
